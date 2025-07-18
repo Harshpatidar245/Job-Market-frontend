66f93e334a30e0eb7e556e800a0d2cd5c03ab018
harshpatidar245 <harshpatidar2400@gmail.com>
2025-07-18 23:21:52 +0530

    update commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all', // Allows all external hosts like ngrok
+    allowedHosts: 'all', // Allows external hosts like ngrok or LAN IP
+    proxy: {
+      '/api': {
+        target: `${process.env.REACT_APP_API_URL}`, // Your backend server
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: strip '/api' if needed
+      },
+    },
   },
 })
