1e8b01bc38bd4765cd551d41122c2947af33f4b5
harshpatidar245 <harshpatidar2400@gmail.com>
2025-07-18 23:41:19 +0530

    update commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     allowedHosts: 'all', // Allows external hosts like ngrok or LAN IP
     proxy: {
