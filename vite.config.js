e39d2112fbd028809a4b83d009d7194075db99f2
harshpatidar245 <harshpatidar2400@gmail.com>
2025-07-19 13:01:44 +0530

    update BACKEND URL

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
- 
-  server: {
-    allowedHosts: 'all', // Allows external hosts like ngrok or LAN IP
-    proxy: {
-      '/api': {
-        target: `${import.meta.env.BACKEND_URL}`, // Your backend server
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: strip '/api' if needed
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: 'all',
+      proxy: {
+        '/api': {
+          target: env.BACKEND_URL,
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  }
 })
